4d197b9419fcf4269e67dce460bc47749ac91a80
Iago <iagocesarts@hotmail.com>
2023-12-05 09:12:31 -0300

    Upload para continualção.

--- frontend/src/Pages/Pokedex/index.tsx
+++ frontend/src/Pages/Pokedex/index.tsx
@@ -1,5 +1,17 @@
+import { NavButton, NavOptionsDiv, PokeNav, PokedexDiv } from "./style";
+
 export default function Pokedex() {
     return (
-        <></>
+        <PokedexDiv>
+            <PokeNav>
+                <p>Tipos de Pesquisa</p>
+                <NavOptionsDiv>
+                    <NavButton>Movimento</NavButton>
+                    <NavButton>Tipo</NavButton>
+                    <NavButton></NavButton>
+                    <NavButton>Teste de Personalidade</NavButton>
+                </NavOptionsDiv>
+            </PokeNav>
+        </PokedexDiv>
     );
 }
--- frontend/src/Pages/Pokedex/style.tsx
+++ frontend/src/Pages/Pokedex/style.tsx
@@ -1,6 +1,44 @@
 import styled from "styled-components";
+import { global } from "../../global";
 
 export const PokedexDiv = styled.div `
     display: flex;
     width: 100%;
+    height: 100%;
+
+    overflow-x: hidden;
+
+    background-color: white;
+`;
+
+/*----------------------------------------------------------------------------*/
+
+export const PokeNav = styled.nav `
+    display: grid;
+    grid-template-columns: 0.2fr 0.4fr auto;
+    width: 100%;
+    height: 50px;
+
+    padding: 1% 2%;
+
+    align-items: center;
+
+    background-color: ${global.cores.amarelo};
+`;
+
+export const NavText = styled.p `
+`;
+
+export const NavOptionsDiv = styled.div `
+    display: flex;
+    width: 100%;
+
+    justify-content: space-between;
+`;
+
+export const NavButton = styled.button `
+    width: 10%;
+
+    text-align: center;
+    font-family: ${global.fonts.quantico};
 `;
